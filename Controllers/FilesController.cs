bcdcfaaa8bf018276314c441acc2ff92e414ce31
elza <elza_carciu@yahoo.com>
2023-10-10 20:57:58 +1100

    added files Controllers

---
+++ Controllers/FilesController.cs
@@ -0,0 +1,40 @@
+
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.VisualBasic;
+
+[ApiController]
+[Route("api/files")]
+public class FilesController : ControllerBase
+{
+
+ private readonly FileExtensionContentTypeProvider _fileExtCntProvider;
+
+ public FilesController(FileExtensionContentTypeProvider fileExtCntProvider)
+ {
+    _fileExtCntProvider = fileExtCntProvider ?? throw new System.ArgumentNullException(nameof(fileExtCntProvider));
+ }
+ 
+ [HttpGet("{fileId}")]
+  public ActionResult GetFile(string fileId)
+  {
+
+    var filePath = "/Users/elzacirciu/Documents/Development/net_workspace/visual_studio_mac/CityInfoAPI/CityInfoAPI/asp_payment_summary_30k.pdf";
+
+    if (!System.IO.File.Exists(filePath))
+    {
+        return NotFound();
+    }
+    
+    var bytes = System.IO.File.ReadAllBytes(filePath);
+
+    if (!_fileExtCntProvider.TryGetContentType(filePath, out var contentType))
+    {
+       contentType = "application/octet-stream";
+    }
+
+
+     return File(bytes, contentType, Path.GetFileName(filePath));
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.StaticFiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +14,7 @@ builder.Services.AddControllers( options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 
 var app = builder.Build();
 
