b527c44769251c071ebbd6de7c4f4d25605380ae
elza <elza_carciu@yahoo.com>
2023-10-20 21:48:33 +1100

    updateting points of interest

--- Controllers/PointsOfInterestController.cs
+++ Controllers/PointsOfInterestController.cs
@@ -1,4 +1,5 @@
 
+using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 
 [Route("api/cities/{cityId}/pointsofinterest")]
@@ -18,7 +19,7 @@ public class PointsOfInterestController : ControllerBase
        return Ok(cityFound.PointsOfInterest);
     }
 
-     [HttpGet("{pointOfInterestId}")]
+     [HttpGet("{pointOfInterestId}", Name = "GetPointOfInterest")]
     public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointOfInterestId)
     {
        var cityFound = CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == cityId);
@@ -37,5 +38,66 @@ public class PointsOfInterestController : ControllerBase
        }
 
        return Ok(pointFound);
+    }
+
+   [HttpPost]
+    public ActionResult CreatePointOfInterest(int cityId, PointOfInterestCreationDto pointOfInterest)
+    {
+      //content assumed to come [FromBody]
+
+      //check the city exists
+      var city = CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == cityId);
+
+      if (city == null)
+      {
+         return NotFound();
+      }
+
+      var maxPointOfInterest = CitiesDataStore.Current.Cities.SelectMany(city => city.PointsOfInterest).Max(p => p.Id);
+
+      var addedPointOfInterest = new PointOfInterestDto()
+      {
+         Id = ++maxPointOfInterest,
+         Name = pointOfInterest.Name,
+         Description = pointOfInterest.Description
+      };
+
+      city.PointsOfInterest.Add(addedPointOfInterest);
+
+      return CreatedAtRoute("GetPointOfInterest", 
+        new 
+        {
+           cityId = cityId,
+           pointOfInterestId = addedPointOfInterest.Id
+        },
+        addedPointOfInterest);
+
+    }
+
+   [HttpPut("{pointofinterestid}")]
+    public ActionResult UpdatePointOfInterest(int cityId, int pointOfInterestId, PointOfInterestUpdateDto pointOfInterestUpdateDto)
+    {
+      //check the city exists
+      var city = CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == cityId);
+
+      if (city == null)
+      {
+         return NotFound();
+      }
+
+      //find point of interest
+      var pointOfInterestFound = city.PointsOfInterest.FirstOrDefault(pInterest => pInterest.Id == pointOfInterestId);
+
+      if (pointOfInterestFound == null)
+      {
+         return NotFound();
+      }
+
+      //set all fields on the point of interest found
+      pointOfInterestFound.Name = pointOfInterestUpdateDto.Name;
+      pointOfInterestFound.Description = pointOfInterestUpdateDto.Description;
+
+      return NoContent();
+
     }
 }
